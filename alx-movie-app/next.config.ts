ce3799a2b51c77ea85ad05b3bc26611878938dd7
Tayo <adetayosunmonu@gmail.com>
2025-08-08 10:56:48 +0100

    setting up movie app project

---
+++ alx-movie-app/next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+   images: {
+    domains: ["m.media-amazon.com"],
+  },
+  /* config options here */
+  reactStrictMode: true,
+};
+
+export default nextConfig;
